273e80fb15ecc662a6ef9352c75d13749cf2b339
Konrad Rieck <rieck@tu-berlin.de>
2025-03-31 17:51:01 +0200

    first working test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,27 @@
-import os
 import pytest
 from reclaim.commands import load
-from reclaim.utils import set_api_key
+from reclaim.utils import set_api_key, id_to_str
+from reclaim_sdk.resources.task import Task
 
-
-@pytest.fixture
-def setup_reclaim():
-    """Setup Reclaim API key"""
-    set_api_key({})
 
 @pytest.fixture
 def commands():
-    """Return all commands"""
-    return load()
+    """Return all available Reclaim CLI commands."""
+    set_api_key({})
+    return {cmd.name: cmd for cmd in load()}
+
+
+@pytest.fixture
+def test_task():
+    """Create and yield a test task, cleaning up afterwards."""
+    # Create task
+    task = Task(title="Test task")
+    tid = id_to_str(task.id)
+    task.save()
+    yield tid
+
+    # Cleanup
+    try:
+        task.delete()
+    except Exception:
+        pass
--- tests/test_delete.py
+++ tests/test_delete.py
@@ -4,20 +4,18 @@
 # Test cases for delete command
 
 import pytest
-from reclaim_sdk.resources.task import Task
-from reclaim.utils import id_to_str, get_task
+from reclaim.utils import get_task
 
-def test_delete_task(setup_reclaim, commands):
-    # Create a test task first
-    task = Task(title="Test task")
-    task.save()
-    task_id = id_to_str(task.id)
 
-    # Get delete-task command
-    cmd = next(cmd for cmd in commands if cmd.name == "delete-task")
-    args = type('Args', (), {'id': task_id})
+def test_delete_task(commands, test_task):
+    """ Test delete-task command """
+    tid = test_task
+    cmd = commands['delete-task']
+
+    # Run command
+    args = type('Args', (), {'id': tid})
     cmd.run(args)
 
     # Verify task is deleted
     with pytest.raises(Exception):
-        get_task(task_id)
+        get_task(tid)
